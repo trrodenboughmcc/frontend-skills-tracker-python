48d724e813f52381f14203057e8190d5e9c97d01
Alex Shaw <alexandergshaw@gmail.com>
2025-04-07 11:40:03 -0500

    adding render configuration files

--- app.py
+++ app.py
@@ -45,4 +45,6 @@ def debug():
     return tracker
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    from os import environ
+    port = int(environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
